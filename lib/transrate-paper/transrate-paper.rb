7d29ff80bdc0b3f8bc50586f318196293c4822f1
Chris Boursnell <cboursnell@gmail.com>
2014-07-23 18:25:04 +0100

    fix installing dependencies and downloading data

--- lib/transrate-paper/transrate-paper.rb
+++ lib/transrate-paper/transrate-paper.rb
@@ -13,7 +13,7 @@ module Transrate_Paper
 
     def install_dependencies
       puts "Checking dependencies"
-      gem_dir = Gem.loaded_specs['pipeline'].full_gem_path
+      gem_dir = Gem.loaded_specs['transrate-paper'].full_gem_path
       gem_deps = File.join(gem_dir, 'deps', 'deps.yaml')
       Bindeps.require gem_deps
     end
@@ -24,10 +24,12 @@ module Transrate_Paper
       if !Dir.exist?("data")
         Open3.capture3 "mkdir data"
       end
+      puts "Downloading data from SRA..."
       hash.each do |key, list|
         list.each do |file|
           if !File.exist?("data/#{file}")
-            cmd = "wget #{file}"
+            cmd = "wget #{file} -P data"
+            puts cmd
             stdout, stderr, status = Open3.capture3 cmd
             if !status.success?
               raise RuntimeError.new("Downloading #{file} from the SRA failed")
@@ -40,6 +42,7 @@ module Transrate_Paper
           end
         end
       end
+      puts "Done"
     end
   end
 end
