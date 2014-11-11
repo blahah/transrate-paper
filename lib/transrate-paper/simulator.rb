f183db7a8d8a53b866f1d8861620a2e33fd339ca
Chris Boursnell <cboursnell@gmail.com>
2014-11-11 10:39:31 +0000

    reads are simulated with expression

--- lib/transrate-paper/simulator.rb
+++ lib/transrate-paper/simulator.rb
@@ -1,70 +1,59 @@
 module TransratePaper
 
   require 'bio'
+  require 'distribution'
 
   class Simulator
 
-    def initialize reference, density, read_length, fragment_size, fragment_sd
+    def initialize reference, read_length, fragment_size, fragment_sd
       @reference = reference
-      @density = density
       @read_length = read_length
       @fragment_size = fragment_size
       @fragment_sd = fragment_sd
-      @random = Random.new(1337)
+      Kernel.srand(1337)
     end
 
     # create two fastq files
     def simulate prefix
       transcriptome = Bio::FastaFormat.open(@reference)
-
+      rng = Distribution::Exponential.rng(0.5)
       count = 0
-      File.open("#{prefix}_1.fastq", "w") do |left|
-        File.open("#{prefix}_2.fastq", "w") do |right|
-          transcriptome.each do |entry|
-            key = entry.definition.split(/\s/).first
-            transcripts = [entry.seq, entry.seq.tr("ACGT", "TGCA").reverse]
-            transcripts.each do |transcript|
-              (0..transcript.length-@read_length).step(@density).each do |pos|
-                fragment_size = (@fragment_size + ((@random.rand - 0.5) * @fragment_sd)).round(0)
-                if pos + fragment_size < transcript.length
-                  bases = transcript[pos..(pos + fragment_size - 1)]
-                  # puts "fragment_size:#{fragment_size}\tpos:#{pos}\ttranscript.length:#{transcript.length}"
-                  # read 1
-                  left.puts "@read:#{count}:#{key}/1"
-                  left.puts bases[0..@read_length-1]
-                  left.puts "+"
-                  left.puts "I"*@read_length
-                  # read 2
-                  right.puts "@read:#{count}:#{key}/2"
-                  right.puts bases[-@read_length..-1].tr("ACGT", "TGCA").reverse
-                  right.puts "+"
-                  right.puts "I"*@read_length
-                  #
-                  count+=1
-                elsif transcript.length - pos > @read_length * 1.3
-                  fragment_size = transcript.length - pos
-                  bases = transcript[pos..(pos + fragment_size - 1)]
-                  # puts "fragment_size:#{fragment_size}\tpos:#{pos}\ttranscript.length:#{transcript.length}"
-                  # read 1
-                  left.puts "@read:#{count}:#{key}/1"
-                  left.puts bases[0..@read_length-1]
-                  left.puts "+"
-                  left.puts "I"*@read_length
-                  # read 2
-                  right.puts "@read:#{count}:#{key}/2"
-                  right.puts bases[-@read_length..-1].tr("ACGT", "TGCA").reverse
-                  right.puts "+"
-                  right.puts "I"*@read_length
-                  #
-                  count+=1
-                end # if
-              end # each pos
-            end # each transcript
-          end
-        end # close right
-      end # close left
+      transcriptome.each do |entry|
+        key = entry.definition.split(/\s/).first
+        expr = (Math.exp(rng.call) * entry.seq.length * 0.01).to_i
+        expr = [expr, 10 * entry.seq.length / @read_length].max
+        simulate_reads_with_expr(entry.seq, expr, key, prefix)
+      end
 
     end # simulate
+
+    def simulate_reads_with_expr(seq, expr, name, prefix)
+      left = File.open(prefix + '.l.fq', 'a')
+      right = File.open(prefix + '.r.fq', 'a')
+      rng = Distribution::Normal.rng(@fragment_size, @fragment_sd)
+      read_length = @read_length
+      read_length = seq.length - 1 if read_length >= seq.length
+      expr.times do |count|
+        fragment = rng.call.to_i
+        fragment = (seq.length - 1) if fragment >= seq.length
+        fragment = read_length + 1 if fragment < read_length
+        len = seq.length
+        pos = (rand * (len - fragment + 1)).to_i
+        bases = seq[pos..(pos + fragment - 1)]
+        # read 1
+        left.puts "@read:#{count}:#{name}/1"
+        left.puts bases[0..(read_length - 1)]
+        left.puts "+"
+        left.puts "I" * read_length
+        # read 2
+        right.puts "@read:#{count}:#{name}/2"
+        right.puts bases[(-read_length)..-1].tr("ACGT", "TGCA").reverse
+        right.puts "+"
+        right.puts "I" * read_length
+      end
+      left.close
+      right.close
+    end
 
   end # class
 
